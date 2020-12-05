33a5cc266e07be4297406fa5302535b6dc44fa63
Filipp Gudis <fgudis@yahoo.com>
2020-12-04 21:11:10 -0800

    sending POST with axios and getting TOKEN back!

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,13 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
 
-export default class Auth extends Component {
+ 
+
+class Auth extends Component {
 
     state = {
         controls: {
@@ -82,6 +86,10 @@ export default class Auth extends Component {
         this.setState({controls: updatedControls});
     }
 
+    submitHandler = (event) => {
+            event.preventDefault();
+            this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    }
     render () {
         const formElementsArray = [];
         for (let key in this.state.controls) {
@@ -105,7 +113,7 @@ export default class Auth extends Component {
         ));
         return (
             <div className = {classes.Auth}>
-                <form>
+                <form onSubmit = {this.submitHandler}>
                  {form}   
                 <Button btnType = 'Success'>Submit</Button> 
                 </form>
@@ -113,3 +121,11 @@ export default class Auth extends Component {
         );
     }
 }
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    };
+};
+
+export default connect (null, mapDispatchToProps)(Auth);
