4bac26ff4bae7f748b329ce57f60250dd7dd30e9
Filipp Gudis <fgudis@yahoo.com>
2020-12-08 13:18:08 -0800

    environment varable used..can find them in config

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import authReducer from './store/reducers/auth';
 //import order from './components/CheckoutSummary/Order';
 
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development'?  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
