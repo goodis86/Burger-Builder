69c48e51f709c65892629e1e3be38181665de3ff
Filipp Gudis <fgudis@yahoo.com>
2021-01-09 20:40:09 -0800

    deleted async component with React.lazy()

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense } from "react"; // suspense has to wrap any code that uses react.lazy!!!
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
-import asyncComponent from "./hoc/asyncComponent/asyncComponent";
+//import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actions from "./store/actions/index";
 
-const asyncCheckout = asyncComponent(() => {
+const Checkout = React.lazy(() => {
   return import("./containers/Checkout/Checkout");
 });
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
   return import("./containers/Orders/Orders");
 });
-const asyncAuth = asyncComponent(() => {
+const Auth = React.lazy(() => {
   return import("./containers/Auth/Auth");
 });
 
 const app = (props) => {
   useEffect(() => {
     props.onTryAutoSignup();
-  }, []);  // we pass an empty array of dependencies to make sure we just render this component once!
-
+  }, []); // we pass an empty array of dependencies to make sure we just render this component once!
 
   let routes = (
     <Switch>
-      <Route path="/auth" component={asyncAuth} />
+      <Route path="/auth" render={() => <Auth/>} />
       <Route path="/" exact component={BurgerBuilder} />
       <Redirect to="/" />
     </Switch>
@@ -36,10 +35,10 @@ const app = (props) => {
   if (props.isAuthenticated) {
     routes = (
       <Switch>
-        <Route path="/checkout" component={asyncCheckout} />
-        <Route path="/orders" component={asyncOrders} />
+        <Route path="/checkout" render={() => <Checkout/>} />
+        <Route path="/orders" render={() => <Orders/>} />
         <Route path="/logout" component={Logout} />
-        <Route path="/auth" component={asyncAuth} />
+        <Route path="/auth" render={() => <Auth/>} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -48,7 +47,9 @@ const app = (props) => {
 
   return (
     <div>
-      <Layout>{routes}</Layout>
+      <Layout>
+        <Suspense fallback={<p>LOading..</p>}>{routes}</Suspense>
+      </Layout>
     </div>
   );
 };
