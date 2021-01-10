95d8d0068e2a8f57b7d42e5c2334e2fe9738ef30
Filipp Gudis <fgudis@yahoo.com>
2021-01-09 22:51:52 -0800

    rest of components converted!

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,15 +1,15 @@
 
 
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './BurgerIngredient.css';
 //import BurgerBuilder from '../../../containers/BurgerBuilder/BurgerBuilder';
 
-class BurgerIngredient extends Component {
-    render() {
+const burgerIngredient = props => {
+   
 
      let ingredient = null;
-    switch (this.props.type) {
+    switch (props.type) {
         case ('bread-bottom'):
             ingredient = <div className = {classes.BreadBottom}></div>;
             break;
@@ -39,10 +39,10 @@ class BurgerIngredient extends Component {
     }
             return ingredient;
         }
-}
 
-    BurgerIngredient.propTypes = {
+
+    burgerIngredient.propTypes = {
         type: PropTypes.string.isRequired
     }
 
-export default BurgerIngredient;
+export default burgerIngredient;
